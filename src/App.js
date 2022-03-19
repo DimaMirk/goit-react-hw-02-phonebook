88e542e32d3e867774cb5a6384a56058e885564d
DimaMirk <dima.myruk@gmail.com>
2022-03-19 23:19:45 +0200

    added main logic for components

--- src/App.js
+++ src/App.js
@@ -1,60 +1,88 @@
 import { Component } from 'react';
-import Statistics from 'components/Statistics';
-import FeedbackOptions from 'components/FeedbackOptions';
-import Section from 'components/Section';
-import Notification from 'components/Notification';
+import { nanoid } from 'nanoid';
 class App extends Component {
+  id = nanoid();
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    visible: false,
+    contacts: [],
+    name: '',
+    number: '',
+    filter: '',
   };
 
-  options = ['good', 'neutral', 'bad'];
+  handleChange = event => {
+    this.setState({ [event.currentTarget.name]: event.currentTarget.value });
+  };
 
-  onIncrement = select => {
-    this.setState(prevState => ({
-      [select]: prevState[select] + 1,
-      visible: true,
+  onSubmite = e => {
+    e.preventDefault();
+    const { number, name } = this.state;
+
+    this.addContacts(name, number);
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  addFilterValue = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  addContacts = (name, number) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, contact],
     }));
   };
 
-  countTotalFeedback() {
-    const total = this.state.good + this.state.neutral + this.state.bad;
-    return total;
-  }
-
-  countPositiveFeedbackPercentage() {
-    const feedback = (100 / this.countTotalFeedback()) * this.state.good;
-    return feedback.toFixed();
-  }
-
   render() {
-    const { good, neutral, bad } = this.state;
     return (
-      <>
-        <Section title={'Please leave feedback '}>
-          <FeedbackOptions
-            options={this.options}
-            onIncrement={this.onIncrement}
+      <section className="formSection">
+        <h1>Phonebook</h1>
+        <form onSubmit={this.onSubmite} className="form">
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            value={this.state.name}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
           />
-        </Section>
-
-        <Section title={'Statistic'}>
-          {this.state.visible ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message={'There is no feedback'} />
-          )}
-        </Section>
-      </>
+          <label htmlFor="number">Number</label>
+          <input
+            id="number"
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+          <button type="submite">Add contact</button>
+        </form>
+        <h1>Contacts</h1>
+        <p>Find contacts by name</p>
+        <input onChange={this.addFilterValue} />
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
+        </ul>
+      </section>
     );
   }
 }
